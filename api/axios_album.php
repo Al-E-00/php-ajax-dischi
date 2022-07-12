f0be122081d6e345a8582dfd6b3708c2e8d421c7
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-12 18:17:27 +0200

    vue linked to php file

---
+++ api/axios_album.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . "/db/music_db.php";
+
+header('Content-Type: application/json');
+
+echo json_encode([
+    "page" => 1,
+    "total_pages" => 1,
+    "total_items" => 10,
+    "albums" => $data,
+]);
---
+++ index_axios.php
@@ -0,0 +1,22 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Axios call -> Vue use</title>
+
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/css/bootstrap.min.css' integrity='sha512-o/MhoRPVLExxZjCFVBsm17Pkztkzmh7Dp8k7/3JrtNCHh0AQ489kwpfA3dPSHzKDe8YCuEhxXq3Y71eb/o6amg==' crossorigin='anonymous'/>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/esm/axios.min.js' integrity='sha512-4Twfq/Rc10HCBmdtTXVcSIBk/hoOXFX52qE8GZV6PhPLWvdfHkZ+BfbI8bt3HzilD1QyEMUADqRiv5MGoOkZYw==' crossorigin='anonymous'></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+</head>
+<body>
+    <div id="app">
+        {{message}}
+    </div>
+    <script src="vue_sections/vuejs.js"></script>
+</body>
+</html>
