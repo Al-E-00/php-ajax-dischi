2d12d4edafd2b54453dfdc5813b9549e039215d8
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-12 19:44:35 +0200

    Filter created NOT WORKS

--- api/axios_album.php
+++ api/axios_album.php
@@ -1,12 +1,22 @@
 <?php
 
-require __DIR__ . "/../db/music_db.php";
+require "../db/music_db.php";
+
+
+$albumFilter = key_exists('genre', $_GET) ? $_GET['genre'] : "";
+
+$filteredAlbums = $data;
+
+if($albumFilter) {
+    $filteredAlbums = array_filter($data, function ($albums) use ($albumFilter){
+        return $albums["genre"] === $albumFilter;
+    });
+}
 
 header('Content-Type: application/json');
-
 echo json_encode([
     "page" => 1,
     "total_pages" => 1,
     "total_items" => 10,
-    "albums" => $data,
+    "albums" => $filteredAlbums,
 ]);
