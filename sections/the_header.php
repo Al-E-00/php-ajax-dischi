d33ec94eb18b40deb09b4a21b917ddeca20dec61
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-12 16:05:27 +0200

    header component done

--- sections/the_header.php
+++ sections/the_header.php
@@ -7,7 +7,7 @@
 <nav class="navbar bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="<?php include __DIR__ . '/../img/spotify-logo.png'?>" alt="spotify-logo" width="30">
+            <img src="./img/spotify-logo.png" alt="spotify-logo" width="30">
         </a>
     </div>
 </nav>
